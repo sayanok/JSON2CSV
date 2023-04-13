22815dc0165efb5d478f0c4054eed47fbb9bd3d3
sayanok <rng.jhn.osca@gmail.com>
2023-04-13 18:04:43 +0900

    add functionality

--- json2csv/src/App.tsx
+++ json2csv/src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [inputContent, setInputContent] = useState("変換したい内容を入力してください");
+  const [convertedContent, setConvertedContent] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function onClickHandler() {
+    let jsonContent;
+    let newErrorMessage;
+
+    setInputContent(inputContent);
+    if (inputContent === "") {
+      setErrorMessage("入力フォームに何も入力されていません");
+    } else {
+      setErrorMessage("");
+    }
+
+    try {
+      jsonContent = JSON.parse(inputContent);
+    } catch (error) {
+      newErrorMessage = error;
+      setErrorMessage(`${newErrorMessage}`);
+    }
+
+    if (!newErrorMessage) {
+      convert(jsonContent);
+    }
+  }
+
+  function convert(jsonContent: Array<any>) {
+    const header = Object.keys(jsonContent[0]).join(",") + "\n";
+
+    const body = jsonContent
+      // インラインコールバック関数
+      .map(function (d) {
+        return Object.keys(d)
+          .map(function (key) {
+            return d[key];
+          })
+          .join(",");
+      })
+      .join("\n");
+    setConvertedContent(`${header + body}`);
+  }
+
+  function deleteItems() {
+    setInputContent("");
+    setConvertedContent("");
+    setErrorMessage("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>入力フォーム</p>
+      <textarea onChange={(e) => setInputContent(e.target.value)} value={inputContent}></textarea>
+      <button onClick={() => onClickHandler()}>変換</button>
+      <p style={{ color: "red" }}>{errorMessage}</p>
+
+      <p>変換後</p>
+      <textarea value={convertedContent}></textarea>
+
+      <p>
+        入力フォームと変換後フォームの内容を削除する：
+        <button onClick={() => deleteItems()}>クリア</button>
+      </p>
+    </>
   );
-}
+};
 
 export default App;
